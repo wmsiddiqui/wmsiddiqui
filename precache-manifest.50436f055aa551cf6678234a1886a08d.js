60344ddfa2e64006a9bc214da542e7902cd4671f
wmsiddiqui <wmsiddiqui.6@gmail.com>
2019-10-06 18:20:52 -0700

    Updates

---
+++ precache-manifest.50436f055aa551cf6678234a1886a08d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fcf3ae36dcdb31d91e3aa7a66bc2b1e",
+    "url": "/wmsiddiqui/index.html"
+  },
+  {
+    "revision": "d233ff24fc18a778ec99",
+    "url": "/wmsiddiqui/static/css/main.197b0ada.chunk.css"
+  },
+  {
+    "revision": "04ff36ab68dc4665c4a0",
+    "url": "/wmsiddiqui/static/js/2.a82b454d.chunk.js"
+  },
+  {
+    "revision": "d233ff24fc18a778ec99",
+    "url": "/wmsiddiqui/static/js/main.09813422.chunk.js"
+  },
+  {
+    "revision": "2c3e0b0b83f5573b506a",
+    "url": "/wmsiddiqui/static/js/runtime~main.534ed890.js"
+  }
+]);
