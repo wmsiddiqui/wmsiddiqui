fe9cc1b03ba3b1f5bea2a99fbec6419e74aa2208
wmsiddiqui <wmsiddiqui.6@gmail.com>
2019-10-06 22:59:31 -0700

    Added content for home page

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
 export default function() {
-	return <div>This is the home page</div>;
+	return (
+		<div>
+			<h1>Waqar Siddiqui</h1>
+			<p>
+				Welcome to my website! This website was created using React and Redux. I've used the GitHub APIs to
+				display some up to date information about my projects. The code is then built and hosted on GitHub
+				pages. At the time of making this website, Redux was probably not needed, since I really don't need a
+				central store at the moment. I also have a few projects written in Node.js and C# which you can check
+				out on my GitHub. Just click the "Projects" link in the navigation bar and check them out!
+			</p>
+		</div>
+	);
 }
